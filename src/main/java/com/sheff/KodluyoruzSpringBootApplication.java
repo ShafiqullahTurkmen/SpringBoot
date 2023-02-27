be59698f2159a6f7cbfcc6db704b7852bab44e0e
Shafiqullah Türkmen <shafiqullah.turkmen@alo-tech.com>
2023-02-27 23:44:03 +0300

    gitignore updated, SpringBootApplication Security exlueded

--- src/main/java/com/sheff/KodluyoruzSpringBootApplication.java
+++ src/main/java/com/sheff/KodluyoruzSpringBootApplication.java
@@ -3,7 +3,10 @@ package com.sheff;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+)
 public class KodluyoruzSpringBootApplication {
 
     public static void main(String[] args) {
