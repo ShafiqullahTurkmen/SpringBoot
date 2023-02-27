75ff50405f2c5d23a196e9ab4e1d971db781a456
Shafiqullah Türkmen <shafiqullah.turkmen@alo-tech.com>
2023-02-27 22:34:34 +0300

    first commit

---
+++ src/main/java/com/sheff/KodluyoruzSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.sheff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodluyoruzSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodluyoruzSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sheff/KodluyoruzSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodluyoruzSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
