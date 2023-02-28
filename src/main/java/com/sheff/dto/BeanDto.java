db986034402ffce09578cbdf3381378ad5cabcbd
Shafiqullah Türkmen <shafiqullah.turkmen@alo-tech.com>
2023-02-28 20:40:48 +0300

    bean configuration added

---
+++ src/main/java/com/sheff/dto/BeanDto.java
@@ -0,0 +1,23 @@
+package com.sheff.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Log4j2
+@Builder
+public class BeanDto {
+    private Long id;
+    private String beanName;
+    private String beanData;
+
+    // başlangıç
+
+
+    //
+}
