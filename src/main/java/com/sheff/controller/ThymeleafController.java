6198a8b21c9a5b38920c51498b29735eaaba6379
Shafiqullah Türkmen <shafiqullah.turkmen@alo-tech.com>
2023-03-03 21:29:10 +0300

    Model,Thymeleaf, css, js added

--- src/main/java/com/sheff/controller/ThymeleafController.java
+++ src/main/java/com/sheff/controller/ThymeleafController.java
@@ -1,8 +1,8 @@
 package com.sheff.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ThymeleafController {
@@ -12,5 +12,22 @@ public class ThymeleafController {
   //  @ResponseBody  is used to return body response, otherwise return html page
     public String getThymeleaf1() {
         return "thymeleaf1";
+    }
+
+    // http://localhost:8080/thymeleaf2
+    @GetMapping("/thymeleaf2")
+    public String getThymeleaf12Model(Model model) {
+        model.addAttribute("key_model1", "I come from model-1");
+        model.addAttribute("key_model2", "I come from model-2");
+        return "thymeleaf1";
+    }
+
+
+    // http://localhost:8080/thymeleaf3
+    @GetMapping("/thymeleaf3")
+    public String getThymeleaf13Model(Model model) {
+        model.addAttribute("key_model1", "I come from model-1");
+        model.addAttribute("key_model2", "I come from model-2");
+        return "/thymeleaf_file/thymeleaf3";
     }
 }
